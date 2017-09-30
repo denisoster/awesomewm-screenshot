7877b992ccfeeed1cc1736580e4e022939cbabc3
denisoster <denis.oster.dev@gmail.com>
2017-09-30 17:52:02 +0300

    refactoring code

--- screenshot.lua
+++ screenshot.lua
@@ -1,45 +1,41 @@
 local awful = require("awful")
 local naughty = require("naughty")
 
-screenshot = os.getenv("HOME") .. "/Pictures/$(date +%F_%T).png"
+timers = { 5,10 }
+screenshot = os.getenv("HOME") .. "/Pictures/scrot/$(date +%F_%T).png"
 
 function scrot_full()
-    local cmd = "scrot " .. screenshot .. " -e 'xclip -selection c -t image/png < $f'"
-    awful.util.spawn_with_shell(cmd)
-    naughty.notify({
-        text = "Take a screenshot of entire screen",
-        timeout = 0.5
-    })
+    scrot("scrot " .. screenshot .. " -e 'xclip -selection c -t image/png < $f', scrot_callback", scrot_callback, "Take a screenshot of entire screen")
 end
 
 function scrot_selection()
-    local cmd = "sleep 0.5 && scrot -s " .. screenshot .. " -e 'xclip -selection c -t image/png < $f'"
-    awful.util.spawn_with_shell(cmd)
-    naughty.notify({
-        text = "Take a screenshot of selection",
-        timeout = 0.5
-    })
+    scrot("sleep 0.5 && scrot -s " .. screenshot .. " -e 'xclip -selection c -t image/png < $f'", scrot_callback, "Take a screenshot of selection")
 end
 
 function scrot_window()
-    local cmd = "scrot -u " .. screenshot .. " -e 'xclip -selection c -t image/png < $f'"
-    awful.util.spawn_with_shell(cmd)
-    naughty.notify({
-        text = "Take a screenshot of focused window",
-        timeout = 0.5
-    })
+    scrot("scrot -u " .. screenshot .. " -e 'xclip -selection c -t image/png < $f'", scrot_callback, "Take a screenshot of focused window")    
 end
 
 function scrot_delay()
-    local cmd5 = "scrot -d 5 " .. screenshot .. " -e 'xclip -selection c -t image/png < $f'"
-    local cmd10 = "scrot -d 10 " .. screenshot .. " -e 'xclip -selection c -t image/png < $f'"
-    awful.menu.new({
-        items = { { "5", awful.util.spawn_with_shell(cmd5) },
-            { "10", awful.util.spawn_with_shell(cmd10) },
-        }
-    }):show({keygrabber= true})
+    items={}
+    for key, value in ipairs(timers)  do
+        items[#items+1]={tostring(value) , "scrot -d ".. value.." " .. screenshot .. " -e 'xclip -selection c -t image/png < $f'","Take a screenshot of delay" }
+    end
+    awful.menu.new(
+    {
+        items = items
+    }
+    ):show({keygrabber= true})
+    scrot_callback()
+end
+
+function scrot(cmd , callback, args)
+    awful.util.spawn_with_shell(cmd)
+    callback(args)
+end
+function scrot_callback(text)
     naughty.notify({
-        text = "Take a screenshot of delay",
+        text = text,
         timeout = 0.5
     })
 end
